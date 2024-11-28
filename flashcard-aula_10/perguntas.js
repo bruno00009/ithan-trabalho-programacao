0fbf01a3b0ae281ccfcb0d0829237130d30050fb
bruno00009 <161394551+bruno00009@users.noreply.github.com>
2024-11-27 22:03:01 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'quem ou eu?',
+    'ithan do capa'
+)
+
+criaCartao(
+    'Geografia',
+    'meu trabalho e?',
+    'vendedor'
+)
+
+criaCartao(
+    'Programação',
+    'O que eu mai gosto no meu trabalho ?',
+    'limpar as prateleiras'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
